343cd195b2a3f133141ecc7d88e079d814de8bc3
David-Neza <tuyishmirend@gmail.com>
2022-03-26 20:39:46 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Navbar from './Navbar'
+import HomeContent from './HomeContent'
+
+function App(){
+  return(
+    <>
+      <Navbar />
+      <HomeContent />
+    </>
+  )
+}
+
+export default App
---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar(){
+    return(
+        <div className = "Nav">
+            <ul>
+                <li>Hello, am David Neza</li>
+                <li>Home</li>   
+                <li>Blog</li>
+                <li>Signup</li>
+                <li>Login</li>                         
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar
