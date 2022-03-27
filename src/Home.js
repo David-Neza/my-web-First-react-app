e6084fd2e0e6acc845cc04e203b4b7c556961b7a
David-Neza <tuyishmirend@gmail.com>
2022-03-27 15:57:06 +0200

    Second commit

--- src/Home.js
+++ src/Home.js
@@ -4,8 +4,8 @@ const Home = () =>{
   return (
     <div>
       <div>
-      Hello I'm name is David, a full stack software engineer from Kigali, Rwanda
-           <img  src={David} width="300" height="300" border-radius="30px" alt='David'></img>
+        Hello I'm name is David, a full stack software engineer from Kigali, Rwanda
+          <img  src={David} width="300" height="300" border-radius="30px" alt='David'></img>
       </div>
     </div>
   );
